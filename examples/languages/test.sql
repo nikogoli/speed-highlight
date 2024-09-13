95e6cb09f727aab577e7d339081f339d7e1988a9
nikogoli <k_m_naoto@yahoo.co.jp>
2024-09-13 12:09:03 +0900

    不要なものを削除

--- examples/languages/test.sql
+++
@@ -1,6 +0,0 @@
--- Comment
-SELECT 'Geeks' || ' ' || @test_hello FROM dual;
-INSERT INTO database (geek_id, geek_name) VALUES (5000, 'abc');
-REPLACE('123geeks123', '123', TRUE);
-/* FIXME Multiline
-	Comment */
